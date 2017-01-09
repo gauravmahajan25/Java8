ed5c1a3088ffdddd1173e5559783a19bb45c50a1
gauravmahajan25 <gaurav.mahajan25@gmail.com>
2017-01-09 13:41:45 +0530

    Exception handling changes

---
+++ samples/src/main/java/java8/samples/ExceptionHamdlingExample.java
@@ -0,0 +1,34 @@
+package java8.samples;
+
+import java.util.function.BiConsumer;
+
+public class ExceptionHamdlingExample {
+
+	public static void main(String[] args) {
+		int[] arr1 = { 1, 2, 3, 4 };
+		int key = 0;
+
+		process(arr1, key, wrapperLambda((v, k) -> {
+			System.out.println(v / k);
+		}));
+	}
+
+	private static void process(int[] arr1, int key, BiConsumer<Integer, Integer> consumer) {
+		for (int i : arr1) {
+			consumer.accept(i, key);
+		}
+
+	}
+
+	private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer) {
+		return (v, k) -> {
+			try {
+				consumer.accept(v, k);
+			} catch (ArithmeticException e) {
+				System.out.println("Exception occurred!");
+			}
+		};
+
+	}
+
+}
